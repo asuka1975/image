590ef6e1ee1623012ebfee91a4ed33a63454e027
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2020-07-14 14:17:00 +0900

    remove undefined behavior

--- image.h
+++ image.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_IMAGE_H
 #define INCLUDE_IMAGE_H
 
+#include <cstddef>
 #include <cstdint>
 #include <iterator>
 #include <type_traits>
@@ -19,13 +20,13 @@ namespace aslib {
     > : std::true_type
     {};
 
-    template <class TMemory, class TImage>
+    template <class TImage>
     class image {
     public:
         using iterator = TImage*;
         using const_iterator = const TImage*;
     public:
-        image(std::vector<TMemory>& mem, std::uint32_t start, std::uint32_t length);
+        image(std::vector<std::byte>& mem, std::uint32_t start, std::uint32_t length);
         bool empty() const noexcept;
         std::size_t size() const noexcept;
         iterator begin() noexcept;
@@ -44,7 +45,7 @@ namespace aslib {
         iterator erase(const_iterator position);
         iterator erase(const_iterator first, const_iterator last);
 
-        image<TMemory, TImage>* operator&() = delete;
+        image<TImage>* operator&() = delete;
         static void* operator new(size_t size) = delete;
         static void operator delete(void *ptr) = delete;
         static void* operator new[](size_t size) = delete;
@@ -54,107 +55,104 @@ namespace aslib {
         static void* operator new(std::size_t, void*) = delete;
         static void* operator new[](std::size_t size, void* ptr) noexcept = delete;
     private:
-        std::vector<TMemory>& memory;
+        std::vector<std::byte>& memory;
         std::uint32_t start;
         std::uint32_t count;
     };
 
-    template <class TMemory, class TImage>
-    inline image<TMemory, TImage>::image(std::vector<TMemory>& mem, std::uint32_t start, std::uint32_t count) :
+    template <class TImage>
+    inline image<TImage>::image(std::vector<std::byte>& mem, std::uint32_t start, std::uint32_t count) :
         memory(mem),
         start(start),
         count(count)
     {
-        static_assert(std::is_unsigned<TMemory>::value && std::is_integral<TMemory>::value == true, "TMemory doesn't express memory array");
         static_assert(std::is_trivially_copyable<TImage>::value == true, "TImage is not trivially copyable");
-        static_assert(sizeof(TMemory) <= sizeof(TImage), "TMemories can't express TImage memory layout by size error");
-        static_assert(sizeof(TImage) % sizeof(TMemory) == 0, "TMemories set can't match TImage memory layout");
 
-        if(start + count * sizeof(TImage) / sizeof(TMemory) > memory.size()) memory.resize(start + count * sizeof(TImage) / sizeof(TMemory));
+        if(start + count * sizeof(TImage) > memory.size()) memory.resize(start + count * sizeof(TImage));
     }
 
-    template <class TMemory, class TImage>
-    inline bool image<TMemory, TImage>::empty() const noexcept {
+    template <class TImage>
+    inline bool image<TImage>::empty() const noexcept {
         return !count;
     }
 
-    template <class TMemory, class TImage>
-    inline std::size_t image<TMemory, TImage>::size() const noexcept {
+    template <class TImage>
+    inline std::size_t image<TImage>::size() const noexcept {
         return count;
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::begin() noexcept {
+    template <class TImage>
+    inline typename image<TImage>::iterator image<TImage>::begin() noexcept {
         return reinterpret_cast<TImage*>(memory.data() + start);
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::const_iterator image<TMemory, TImage>::begin() const noexcept {
+    template <class TImage>
+    inline typename image<TImage>::const_iterator image<TImage>::begin() const noexcept {
         return reinterpret_cast<TImage*>(memory.data() + start);
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::end() noexcept {
-        return reinterpret_cast<TImage*>(memory.data() + start + count * sizeof(TImage) / sizeof(TMemory));
+    template <class TImage>
+    inline typename image<TImage>::iterator image<TImage>::end() noexcept {
+        return reinterpret_cast<TImage*>(memory.data() + start + count * sizeof(TImage));
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::const_iterator image<TMemory, TImage>::end() const noexcept {
-        return reinterpret_cast<TImage*>(memory.data() + start + count * sizeof(TImage) / sizeof(TMemory));
+    template <class TImage>
+    inline typename image<TImage>::const_iterator image<TImage>::end() const noexcept {
+        return reinterpret_cast<TImage*>(memory.data() + start + count * sizeof(TImage));
     }
 
-    template <class TMemory, class TImage>
-    inline void image<TMemory, TImage>::push_back(const TImage& obj) {
-        const TMemory* mem = reinterpret_cast<const TMemory*>(&obj);
-        auto c = sizeof(TImage) / sizeof(TMemory);
+    template <class TImage>
+    inline void image<TImage>::push_back(const TImage& obj) {
+        const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
+        auto c = sizeof(TImage);
         memory.insert(memory.begin() + start + count * c, mem, mem + c);
         count++;
     }
 
-    template <class TMemory, class TImage>
-    inline void image<TMemory, TImage>::push_back(TImage&& obj) {
-        const TMemory* mem = reinterpret_cast<const TMemory*>(&obj);
-        auto c = sizeof(TImage) / sizeof(TMemory);
+    template <class TImage>
+    inline void image<TImage>::push_back(TImage&& obj) {
+        const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
+        auto c = sizeof(TImage);
         memory.insert(memory.begin() + start + count * c, mem, mem + c);
         count++;
     }
 
-    template <class TMemory, class TImage>
-    inline void image<TMemory, TImage>::pop_back() {
+    template <class TImage>
+    inline void image<TImage>::pop_back() {
         if(count == 0) return;
-        auto c = sizeof(TImage) / sizeof(TMemory);
+        auto c = sizeof(TImage);
         memory.erase(memory.begin() + start + count * c - c, memory.begin() + start + count * c);
         count--;
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::insert(typename image<TMemory, TImage>::const_iterator position, const TImage& obj) {
-        const TMemory* mem = reinterpret_cast<const TMemory*>(&obj);
+    template <class TImage>
+    inline typename image<TImage>::iterator image<TImage>::insert(typename image<TImage>::const_iterator position, const TImage& obj) {
+        const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
         auto d = position - begin();
-        auto offset = start + d * sizeof(TImage) / sizeof(TMemory);
-        auto c = sizeof(TImage) / sizeof(TMemory);
+        auto offset = start + d * sizeof(TImage);
+        auto c = sizeof(TImage);
         memory.insert(memory.begin() + offset, mem, mem + c);
         count++;
         return memory.begin() + offset;
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::insert(typename image<TMemory, TImage>::const_iterator position, TImage&& obj) {
-        const TMemory* mem = reinterpret_cast<const TMemory*>(&obj);
+    template <class TImage>
+    inline typename image<TImage>::iterator image<TImage>::insert(typename image<TImage>::const_iterator position, TImage&& obj) {
+        const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
         auto d = position - begin();
-        auto offset = start + d * sizeof(TImage) / sizeof(TMemory);
-        auto c = sizeof(TImage) / sizeof(TMemory);
+        auto offset = start + d * sizeof(TImage);
+        auto c = sizeof(TImage);
         memory.insert(memory.begin() + offset, mem, mem + c);
         count++;
         return begin() + d;
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::insert(typename image<TMemory, TImage>::const_iterator position, std::size_t n, const TImage& obj) {
-        const TMemory* mem = reinterpret_cast<const TMemory*>(&obj);
+    template <class TImage>
+    inline typename image<TImage>::iterator image<TImage>::insert(typename image<TImage>::const_iterator position, std::size_t n, const TImage& obj) {
+        const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
         auto d = position - begin();
-        auto offset = start + d * sizeof(TImage) / sizeof(TMemory);
-        auto c = sizeof(TImage) / sizeof(TMemory);
+        auto offset = start + d * sizeof(TImage);
+        auto c = sizeof(TImage);
         for(int i = 0; i < n; i++) {
             memory.insert(memory.begin() + offset, mem, mem + c);
         }
@@ -162,19 +160,19 @@ namespace aslib {
         return begin() + d;
     }
 
-    template <class TMemory, class TImage>
+    template <class TImage>
     template <class InputIterator>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::insert(typename image<TMemory, TImage>::const_iterator position, InputIterator first, InputIterator last) {
+    inline typename image<TImage>::iterator image<TImage>::insert(typename image<TImage>::const_iterator position, InputIterator first, InputIterator last) {
         static_assert(is_iterator<InputIterator>::value == true, "InputIterator is not iteratable");
         static_assert(std::is_same<TImage&, decltype(*first)>::value == true 
             || std::is_same<TImage, decltype(*first)>::value == true
             || std::is_same<const TImage&, decltype(*first)>::value == true, "Iterator content is not TImage");
         auto d = position - begin();
-        auto offset = start + d * sizeof(TImage) / sizeof(TMemory);
-        auto c = sizeof(TImage) / sizeof(TMemory);
+        auto offset = start + d * sizeof(TImage);
+        auto c = sizeof(TImage) ;
         auto i = 0;
         for(auto iter = first; iter != last; iter++) {
-            const TMemory* mem = reinterpret_cast<const TMemory*>(&(*iter));
+            const std::byte* mem = reinterpret_cast<const std::byte*>(&(*iter));
             memory.insert(memory.begin() + offset + i * c, mem, mem + c);
             i++;
         }
@@ -182,29 +180,29 @@ namespace aslib {
         return begin() + d;
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::insert(typename image<TMemory, TImage>::const_iterator position, std::initializer_list<TImage> il) {
+    template <class TImage>
+    inline typename image<TImage>::iterator image<TImage>::insert(typename image<TImage>::const_iterator position, std::initializer_list<TImage> il) {
         return insert(position, il.begin(), il.end());
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::erase(typename image<TMemory, TImage>::const_iterator position) {
+    template <class TImage>
+    inline typename image<TImage>::iterator image<TImage>::erase(typename image<TImage>::const_iterator position) {
         auto d = position - begin();
         if(d >= count) return end();
-        auto offset = start + d * sizeof(TImage) / sizeof(TMemory);
-        auto c = sizeof(TImage) / sizeof(TMemory);
+        auto offset = start + d * sizeof(TImage);
+        auto c = sizeof(TImage);
         memory.erase(memory.begin() + offset, memory.begin() + offset + c);
         count--;
         return begin() + d;
     }
 
-    template <class TMemory, class TImage>
-    inline typename image<TMemory, TImage>::iterator image<TMemory, TImage>::erase(typename image<TMemory, TImage>::const_iterator first, typename image<TMemory, TImage>::const_iterator last) {
+    template <class TImage>
+    inline typename image<TImage>::iterator image<TImage>::erase(typename image<TImage>::const_iterator first, typename image<TImage>::const_iterator last) {
         auto d = first - begin();
         if(d >= count || last - begin() < first - begin()) return end();
         if(last - begin() > count) last = end();
-        auto offset = start + d * sizeof(TImage) / sizeof(TMemory);
-        auto c = sizeof(TImage) / sizeof(TMemory);
+        auto offset = start + d * sizeof(TImage);
+        auto c = sizeof(TImage);
         auto n = last - first;
         memory.erase(memory.begin() + offset, memory.begin() + offset + c * n);
         count -= n;
