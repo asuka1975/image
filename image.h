27f316d3c1c4733ec8f0e72d21705ea64aed7727
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2020-07-16 16:28:10 +0900

    added start() and syncronizing system between images from the same memory.

--- image.h
+++ image.h
@@ -1,9 +1,11 @@
 #ifndef ASLIB_IMAGE_H
 #define ASLIB_IMAGE_H
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <iterator>
+#include <map>
 #include <string>
 #include <sstream>
 #include <type_traits>
@@ -22,6 +24,16 @@ namespace aslib {
     > : std::true_type
     {};
 
+    template <class TImage> class image;
+
+    class image_shared_state {
+        static std::map<std::uintptr_t, std::map<std::uintptr_t, std::uint32_t>> access_info;
+        template <class TImage> 
+        friend class image;
+    };
+
+    std::map<std::uintptr_t, std::map<std::uintptr_t, std::uint32_t>> image_shared_state::access_info;
+
     template <class TImage>
     class image {
     public:
@@ -31,9 +43,11 @@ namespace aslib {
         using const_reference = const TImage&;
     public:
         image(std::vector<std::byte>& mem, std::uint32_t start, std::uint32_t length);
+        ~image();
         bool empty() const noexcept;
         std::size_t size() const noexcept;
         std::size_t occupation() const noexcept;
+        std::uint32_t start() const noexcept;
         iterator begin() noexcept;
         const_iterator begin() const noexcept;
         iterator end() noexcept;
@@ -68,21 +82,29 @@ namespace aslib {
         static void* operator new[](std::size_t size, void* ptr) noexcept = delete;
     private:
         std::vector<std::byte>& memory;
-        std::uint32_t start;
         std::uint32_t count;
+        std::uintptr_t id;
     private:
         bool _M_range_check(std::size_t i) const;
+        void _M_change_count(std::int32_t d);
     };
+
 
     template <class TImage>
     inline image<TImage>::image(std::vector<std::byte>& mem, std::uint32_t start, std::uint32_t count) :
         memory(mem),
-        start(start),
-        count(count)
+        count(count),
+        id(image_shared_state::access_info[reinterpret_cast<std::uintptr_t>(&mem)].size() ? (image_shared_state::access_info[reinterpret_cast<std::uintptr_t>(&mem)].rbegin()->first + 1) : 0)
     {
         static_assert(std::is_trivially_copyable<TImage>::value == true, "TImage is not trivially copyable");
-
+        
+        image_shared_state::access_info[reinterpret_cast<std::uintptr_t>(&mem)][id] = start;
         if(start + count * sizeof(TImage) > memory.size()) memory.resize(start + count * sizeof(TImage));
+    }
+
+    template <class TImage>
+    inline image<TImage>::~image() {
+        image_shared_state::access_info[reinterpret_cast<std::uintptr_t>(&memory)].erase(id);
     }
 
     template <class TImage>
@@ -101,33 +123,38 @@ namespace aslib {
     }
 
     template <class TImage>
+    inline std::uint32_t image<TImage>::start() const noexcept {
+        return image_shared_state::access_info[reinterpret_cast<std::uintptr_t>(&memory)][id];
+    }
+
+    template <class TImage>
     inline typename image<TImage>::iterator image<TImage>::begin() noexcept {
-        return reinterpret_cast<TImage*>(memory.data() + start);
+        return reinterpret_cast<TImage*>(memory.data() + start());
     }
 
     template <class TImage>
     inline typename image<TImage>::const_iterator image<TImage>::begin() const noexcept {
-        return reinterpret_cast<TImage*>(memory.data() + start);
+        return reinterpret_cast<TImage*>(memory.data() + start());
     }
 
     template <class TImage>
     inline typename image<TImage>::iterator image<TImage>::end() noexcept {
-        return reinterpret_cast<TImage*>(memory.data() + start + count * sizeof(TImage));
+        return reinterpret_cast<TImage*>(memory.data() + start() + count * sizeof(TImage));
     }
 
     template <class TImage>
     inline typename image<TImage>::const_iterator image<TImage>::end() const noexcept {
-        return reinterpret_cast<TImage*>(memory.data() + start + count * sizeof(TImage));
+        return reinterpret_cast<TImage*>(memory.data() + start() + count * sizeof(TImage));
     }
 
     template <class TImage>
     inline typename image<TImage>::reference image<TImage>::operator[](std::size_t i) {
-        return *reinterpret_cast<TImage*>(memory.data() + start + i * sizeof(TImage));
+        return *reinterpret_cast<TImage*>(memory.data() + start() + i * sizeof(TImage));
     }
 
     template <class TImage>
     inline typename image<TImage>::const_reference image<TImage>::operator[](std::size_t i) const {
-        return *reinterpret_cast<TImage*>(memory.data() + start + i * sizeof(TImage));
+        return *reinterpret_cast<TImage*>(memory.data() + start() + i * sizeof(TImage));
     }
 
     template <class TImage>
@@ -154,34 +181,34 @@ namespace aslib {
     inline void image<TImage>::push_back(const TImage& obj) {
         const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
         auto c = sizeof(TImage);
-        memory.insert(memory.begin() + start + count * c, mem, mem + c);
-        count++;
+        memory.insert(memory.begin() + start() + count * c, mem, mem + c);
+        _M_change_count(1);
     }
 
     template <class TImage>
     inline void image<TImage>::push_back(TImage&& obj) {
         const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
         auto c = sizeof(TImage);
-        memory.insert(memory.begin() + start + count * c, mem, mem + c);
-        count++;
+        memory.insert(memory.begin() + start() + count * c, mem, mem + c);
+        _M_change_count(1);
     }
 
     template <class TImage>
     inline void image<TImage>::pop_back() {
         if(count == 0) return;
         auto c = sizeof(TImage);
-        memory.erase(memory.begin() + start + count * c - c, memory.begin() + start + count * c);
-        count--;
+        memory.erase(memory.begin() + start() + count * c - c, memory.begin() + start() + count * c);
+        _M_change_count(-1);
     }
 
     template <class TImage>
     inline typename image<TImage>::iterator image<TImage>::insert(typename image<TImage>::const_iterator position, const TImage& obj) {
         const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
         auto d = position - begin();
-        auto offset = start + d * sizeof(TImage);
+        auto offset = start() + d * sizeof(TImage);
         auto c = sizeof(TImage);
         memory.insert(memory.begin() + offset, mem, mem + c);
-        count++;
+        _M_change_count(1);
         return memory.begin() + offset;
     }
 
@@ -189,10 +216,10 @@ namespace aslib {
     inline typename image<TImage>::iterator image<TImage>::insert(typename image<TImage>::const_iterator position, TImage&& obj) {
         const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
         auto d = position - begin();
-        auto offset = start + d * sizeof(TImage);
+        auto offset = start() + d * sizeof(TImage);
         auto c = sizeof(TImage);
         memory.insert(memory.begin() + offset, mem, mem + c);
-        count++;
+        _M_change_count(1);
         return begin() + d;
     }
 
@@ -200,12 +227,12 @@ namespace aslib {
     inline typename image<TImage>::iterator image<TImage>::insert(typename image<TImage>::const_iterator position, std::size_t n, const TImage& obj) {
         const std::byte* mem = reinterpret_cast<const std::byte*>(&obj);
         auto d = position - begin();
-        auto offset = start + d * sizeof(TImage);
+        auto offset = start() + d * sizeof(TImage);
         auto c = sizeof(TImage);
         for(int i = 0; i < n; i++) {
             memory.insert(memory.begin() + offset, mem, mem + c);
         }
-        count += n;
+        _M_change_count(n);
         return begin() + d;
     }
 
@@ -217,7 +244,7 @@ namespace aslib {
             || std::is_same<TImage, decltype(*first)>::value == true
             || std::is_same<const TImage&, decltype(*first)>::value == true, "Iterator content is not TImage");
         auto d = position - begin();
-        auto offset = start + d * sizeof(TImage);
+        auto offset = start() + d * sizeof(TImage);
         auto c = sizeof(TImage) ;
         auto i = 0;
         for(auto iter = first; iter != last; iter++) {
@@ -225,7 +252,7 @@ namespace aslib {
             memory.insert(memory.begin() + offset + i * c, mem, mem + c);
             i++;
         }
-        count += last - first;
+        _M_change_count(last - first);
         return begin() + d;
     }
 
@@ -238,10 +265,10 @@ namespace aslib {
     inline typename image<TImage>::iterator image<TImage>::erase(typename image<TImage>::const_iterator position) {
         auto d = position - begin();
         if(d >= count) return end();
-        auto offset = start + d * sizeof(TImage);
+        auto offset = start() + d * sizeof(TImage);
         auto c = sizeof(TImage);
         memory.erase(memory.begin() + offset, memory.begin() + offset + c);
-        count--;
+        _M_change_count(-1);
         return begin() + d;
     }
 
@@ -250,17 +277,28 @@ namespace aslib {
         auto d = first - begin();
         if(d >= count || last - begin() < first - begin()) return end();
         if(last - begin() > count) last = end();
-        auto offset = start + d * sizeof(TImage);
+        auto offset = start() + d * sizeof(TImage);
         auto c = sizeof(TImage);
         auto n = last - first;
         memory.erase(memory.begin() + offset, memory.begin() + offset + c * n);
-        count -= n;
+        _M_change_count(-n);
         return begin() + d;
     }
 
     template <class TImage>
     inline bool image<TImage>::_M_range_check(std::size_t i) const {
         return i < size();
+    }
+
+    template <class TImage>
+    inline void image<TImage>::_M_change_count(std::int32_t d) {
+        count += d;
+        auto& access = image_shared_state::access_info[reinterpret_cast<std::uintptr_t>(&memory)];
+        for(auto[k, v] : access) {
+            if(v >= start() && k != id) {
+                access[k] += d * sizeof(TImage);
+            }
+        }
     }
 }
 
